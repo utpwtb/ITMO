1737b641cfc6222d1287d412e59291ac3b1abf6f
utpwtb <147993683+utpwtb@users.noreply.github.com>
2025-10-21 00:26:09 +0300

    update
    update

--- 3 Язык Программирования/task/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ 3 Язык Программирования/task/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e629915f2773211a9375efff6b87cb1e1cf4bc5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1094df97fb67ee711491f0c5ba496cafa0f8199b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
