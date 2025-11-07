46bdb623fec7cadad83fced5cfa6d3747ca35317
utpwtb <147993683+utpwtb@users.noreply.github.com>
2025-11-07 20:41:53 +0300

    update
    update

---
+++ 3 Язык Программирования/task/Task5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static async Task Main()
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        Console.WriteLine($"Главный поток: {Environment.CurrentManagedThreadId}");
+
+        string file1 = "C:\\develop\\NOTE_UTP\\StudyNote\\3 Язык Программирования\\task\\Task5\\file\\file1.txt";
+        string file2 = "C:\\develop\\NOTE_UTP\\StudyNote\\3 Язык Программирования\\task\\Task5\\file\\file2.txt";
+        string file3 = "C:\\develop\\NOTE_UTP\\StudyNote\\3 Язык Программирования\\task\\Task5\\file\\file3.txt";
+
+        var task1 = CountWordsAsync(file1);
+        var task2 = CountWordsAsync(file2);
+        var task3 = CountWordsAsync(file3);
+
+        await Task.WhenAll(task1, task2, task3);
+
+        stopwatch.Stop();
+        Console.WriteLine($"\nОбщее время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+    }
+
+    static async Task CountWordsAsync(string filePath)
+    {
+        var sw = Stopwatch.StartNew();
+
+        Console.WriteLine($"Начало обработки файла: {filePath}");
+        string text = await File.ReadAllTextAsync(filePath);
+        int wordCount = text.Split([' ', '\n', '\r', '\t'], StringSplitOptions.RemoveEmptyEntries).Length;
+
+        sw.Stop();
+        Console.WriteLine($"Файл: {Path.GetFileName(filePath)}, слов: {wordCount}, поток: {Environment.CurrentManagedThreadId}, время: {sw.ElapsedMilliseconds} мс");
+    }
+}
--- 3 Язык Программирования/task/Task5/obj/Debug/net9.0/Task5.AssemblyInfo.cs
+++ 3 Язык Программирования/task/Task5/obj/Debug/net9.0/Task5.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0f844579bad46b21b6076ad4e96d22a1a9e14e")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task5")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
